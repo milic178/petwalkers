173d2186b3ed09c7419d88fae0a51726979c4382
milic178 <marko.milic178@gmail.com>
2017-05-19 21:29:16 +0200

    fixed menu for users,guests, and admins

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -25,40 +25,65 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'Petwalkers',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $navItems=[
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'Status', 'url' => ['/status/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']]
-    ];
-    if (Yii::$app->user->isGuest) {
-        array_push($navItems,['label' => 'Sign In', 'url' => ['/user/login']],
-            ['label' => 'Sign Up', 'url' => ['/user/register']]);
-    }
-    else {
-        array_push($navItems,
-            ['label' => 'My Profile', 'url' => ['/user/settings/profile']],
-            ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-            'url' => ['/site/logout'],'linkOptions' => ['data-method' => 'post']]);
-    }
-echo Nav::widget([
-    'options' => ['class' => 'navbar-nav navbar-right'],
-    'items' => $navItems,
-]);
+       <?php
+       NavBar::begin([
+           'brandLabel' => 'Petwalkers',
+           'brandUrl' => Yii::$app->homeUrl,
+           'options' => [
+               'class' => 'navbar-inverse navbar-fixed-top',
+           ],
+       ]);
 
-    /**
-     * end of user module
-     */
-    NavBar::end();
-    ?>
+/**
+* Constructing menu array if
+*/
+       if (Yii::$app->user->isGuest) {
+           $navItems=[
+               ['label' => 'Sign In', 'url' => ['/user/login']],
+               ['label' => 'Sign Up', 'url' => ['/user/register']]
+           ];
+       }
+       else {
+           $navItems=[
+               ['label' => 'Home', 'url' => ''.Yii::$app->homeUrl.''],
+               ['label' => 'Manage Adds', ''.Yii::$app->homeUrl.'' => ['#'],'visible' => Yii::$app->user->identity->isAdmin],
+               ['label' => 'Manage Users', 'url' => ['/user/admin/index'],'visible' => Yii::$app->user->identity->isAdmin],
+               ['label' => ' '. Yii::$app->user->identity->username .' ',
+                   'items' =>
+                   [
+                        ['label' => 'My Profile', 'url' => '/user/settings/profile'],
+                        ['label' => 'My adds', 'url' => '#'],
+                        ['label' => 'Logout', 'url' => '/site/logout','linkOptions' => ['data-method' => 'post']],
+                   ]
+               ]
+
+           ];
+       }
+/**
+* Right top menu
+*/
+       echo Nav::widget([
+           'options' => ['class' => 'navbar-nav navbar-right'],
+           'items' =>$navItems,
+       ]);
+/**
+* Left top menu
+*/
+       echo Nav::widget([
+           'options' => ['class' => 'navbar-nav navbar-left'],
+           'items' =>[
+               ['label' => 'About', 'url' => ['/site/about']],
+               ['label' => 'Contact', 'url' => ['/site/contact']]
+           ]
+       ]);
+
+       NavBar::end();
+
+
+
+
+       ?>
+
 
     <div class="container">
         <?= Breadcrumbs::widget([
