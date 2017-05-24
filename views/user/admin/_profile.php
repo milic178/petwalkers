47149c81f02cf7804a571b7baa147bcc04a04413
milic178 <marko.milic178@gmail.com>
2017-05-24 23:43:35 +0200

    added few models

--- views/user/admin/_profile.php
+++ views/user/admin/_profile.php
@@ -11,6 +11,7 @@
 
 use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
+use kartik\file\FileInput;
 
 /**
  * @var yii\web\View $this
@@ -20,7 +21,17 @@ use yii\helpers\Html;
 ?>
 
 <?php $this->beginContent('@dektrium/user/views/admin/update.php', ['user' => $user]) ?>
+<div class="panel-heading">
+    <h3 class="panel-title" align="center">
+        <?=  Html::img($user->profile->getImageUrl(), [
+            'class'=>'img-thumbnail',
+            'width'=>'40%',
+            'height'=>'40%',
+            'title'=>$user->profile->first_name,
+        ]); ?>
 
+    </h3>
+</div>
 <?php $form = ActiveForm::begin([
     'layout' => 'horizontal',
     'enableAjaxValidation' => true,
@@ -40,16 +51,21 @@ use yii\helpers\Html;
 
 <?= $form->field($profile, 'age')->textInput(['maxlength'=>2]) ?>
 
-<?= $form->field($profile, 'avatar_photo')->fileInput()->hint(Yii::t('user', 'Upoload your photo')) ?>
+<?= $form->field($profile, 'avatar_photo')->widget(FileInput::classname(), [
+    'options' => ['accept' => 'image/*'],
+]);  ?>
 
 
 <?= $form->field($profile, 'about_me')->textarea(['rows' => '6','placeholder' =>'Write something about yourself in few words']) ?>
 
 <?= $form->field($profile, 'smoker')->radioList(array('1'=>'Yes',0=>'No')); ?>
 
-<?= $form->field($profile, 'my_animals') ?>
+<?= $form->field($profile, 'my_animals')->textInput(['placeholder' => 'Do you have any pets at home?']) ?>
 
-<?= $form->field($profile, 'social_link') ?>
+<?= $form->field($profile, 'social_link') ->textInput(['placeholder' => 'Url to your social profile (Optional)']) ?>
+
+<?=$form->field($profile, 'first_time')->hiddenInput(['value'=> 1])->label(false)?>
+
 
 <div class="form-group">
     <div class="col-lg-offset-3 col-lg-9">
